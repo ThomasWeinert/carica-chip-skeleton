47b890dfa5cf26f955c1014aa0d3106de6275db7
Thomas Weinert <thomas@weinert.info>
2013-11-04 18:30:42 +0100

    Initial commit, a skeleton for Carica Chip projects

---
+++ bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+require(__DIR__ . '/../vendor/autoload.php');
+
+Carica\Io\Loader::register();
+
+use Carica\Io as Io;
+use Carica\Firmata as Firmata;
+use Carica\Chip as Chip;
+
+if (@include(__DIR__ . '/configuration.php')) {
+  if (!defined('CARICA_FIRMATA_SERIAL_BAUD')) {
+    define('CARICA_FIRMATA_SERIAL_BAUD', 57600);
+  }
+
+  switch (CARICA_FIRMATA_MODE) {
+  case 'tcp':
+    $stream = new Io\Stream\Tcp(
+      CARICA_FIRMATA_TCP_SERVER,
+      CARICA_FIRMATA_TCP_PORT
+    );
+    break;
+  case 'serial-dio':
+    $stream = new Io\Stream\Serial\Dio(
+      CARICA_FIRMATA_SERIAL_DEVICE,
+      CARICA_FIRMATA_SERIAL_BAUD
+    );
+    break;
+  case 'serial':
+    $stream = new Io\Stream\Serial(
+      CARICA_FIRMATA_SERIAL_DEVICE,
+      CARICA_FIRMATA_SERIAL_BAUD
+    );
+    break;
+  default:
+    die('Invalid CARICA_FIRMATA_MODE: '.CARICA_FIRMATA_MODE);
+  }
+
+  return new Firmata\Board($stream);
+
+} else {
+  die('Please copy "dist.configuration.php" to "configuration.php" and adapt the configuration options');
+}
---
+++ server.php
@@ -0,0 +1,14 @@
+<?php
+require(__DIR__.'/vendor/autoload.php');
+
+$board = include(__DIR__.'/bootstrap.php');
+
+$board
+  ->activate()
+  ->done(
+    function () use ($board) {
+      // Start here!
+    }
+  );
+
+Carica\Io\Event\Loop\Factory::run();
